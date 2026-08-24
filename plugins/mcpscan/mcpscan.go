7450918b72ec5f1701784b7bbe1bed9f0ea7cd0e
Jose Meira <90699520+jmeiracorbal@users.noreply.github.com>
2026-08-24 15:46:36 +0200

    chore(release): bump to 0.11.0-beta.3 (#26)

--- plugins/mcpscan/mcpscan.go
+++ plugins/mcpscan/mcpscan.go
@@ -247,7 +247,7 @@ func mcpHandshake(w io.Writer, r io.Reader) ([]string, error) {
 		Params: map[string]interface{}{
 			"protocolVersion": "2024-11-05",
 			"capabilities":    map[string]interface{}{},
-			"clientInfo":      map[string]interface{}{"name": "gtkai", "version": "0.11.0-beta.2"},
+			"clientInfo":      map[string]interface{}{"name": "gtkai", "version": "0.11.0-beta.3"},
 		},
 	}); err != nil {
 		return nil, err
